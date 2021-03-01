e20a6c14cdbd651b97eb6f7e2cbad25c2d8f2743
Gabriel Lucena <gnllucena@gmail.com>
2021-03-01 00:38:38 -0300

    RDS, Secret Manager e VPC.

--- tools/consumer/Function.cs
+++ tools/consumer/Function.cs
@@ -1,21 +1,16 @@
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace Lambda
 {
     public class Function
     {
-        public async Task<string> Handler(SQSEvent evnt, ILambdaContext context)
+        public async Task Handler(SQSEvent evnt, ILambdaContext context)
         {
             context.Logger.LogLine("LOGGING FROM APPLICATION: " + JsonSerializer.Serialize(evnt));
-            
-            var response = string.Join(" ---- ", evnt.Records.Select(x => x.Body));
-
-            return response;
         }
     }
 }
